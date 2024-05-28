285d2d1564158f4bba33723f014e7851acb6f2aa
davib <davi.brantes@gmail.com>
2024-05-27 22:57:05 -0300

    Inicializado projeto

---
+++ src/main/java/com/cristosalvaindaiatuba/gerenciador_escalas_igreja/CristoSalvaGerenciadorDeEscalasApplication.java
@@ -0,0 +1,13 @@
+package com.cristosalvaindaiatuba.gerenciador_escalas_igreja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CristoSalvaGerenciadorDeEscalasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CristoSalvaGerenciadorDeEscalasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cristosalvaindaiatuba/gerenciador_escalas_igreja/CristoSalvaGerenciadorDeEscalasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristosalvaindaiatuba.gerenciador_escalas_igreja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CristoSalvaGerenciadorDeEscalasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
